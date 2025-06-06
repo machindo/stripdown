552850c2e95d2a9aa95910186d38eb939ed4ee5e
Justin Paul Silva <justin@jpsilva.com>
2025-06-06 14:41:49 -0400

    display spec in playground app

--- apps/playground/src/routes/guide.tsx
+++ apps/playground/src/routes/guide.tsx
@@ -1,14 +1,51 @@
+import { html } from '@stripdown/spec/spec.md'
 import { createFileRoute } from '@tanstack/solid-router'
+import { css } from 'styled-system/css'
 import { Container } from 'styled-system/jsx'
+
+const style = css({
+  '& h1': {
+    fontSize: '2xl',
+    fontWeight: 'semibold',
+  },
+  '& h2': {
+    fontSize: 'xl',
+    fontWeight: 'semibold',
+    mt: 3,
+  },
+  '& h3': {
+    fontSize: 'lg',
+    fontWeight: 'semibold',
+    mt: 3,
+  },
+  '& h4': {
+    fontWeight: 'semibold',
+    mt: 3,
+  },
+  '& p': {
+    my: 2,
+  },
+  '& pre': {
+    p: 3,
+    border: '1px solid',
+    borderColor: 'gray.7',
+  },
+  '& ul': {
+    listStyleType: 'disc',
+    paddingInlineStart: 6,
+  },
+  '& ol': {
+    listStyleType: 'decimal',
+    paddingInlineStart: 6,
+  },
+})
+
+const About = () => (
+  <Container py={4}>
+    <div class={style} innerHTML={html} />
+  </Container>
+)
 
 export const Route = createFileRoute('/guide')({
   component: About,
 })
-
-function About() {
-  return (
-    <Container>
-      <h2>Stripdown Guide</h2>
-    </Container>
-  )
-}
--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,5 +1,6 @@
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { defineConfig } from 'vite'
+import { Mode, plugin as markdown } from 'vite-plugin-markdown'
 import solid from 'vite-plugin-solid'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
@@ -8,5 +9,6 @@ export default defineConfig({
     tsconfigPaths(),
     TanStackRouterVite({ target: 'solid', autoCodeSplitting: true }),
     solid(),
+    markdown({ mode: [Mode.HTML, Mode.TOC] }),
   ],
 })
